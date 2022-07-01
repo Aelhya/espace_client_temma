4373c5e30b5731ed817d9b5aa28b05044ae0b8bb
Adélaïde LOUIS <adelaide.louis78@hotmail.fr>
2022-07-01 14:52:46 +0200

    user: page 'test' pour le liste des fichiers

--- espace_client/src/Controller/CategoryController.php
+++ espace_client/src/Controller/CategoryController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\FileRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'app_user_category_index', methods: ['GET'])]
-    public function indexUser(CategoryRepository $categoryRepository, UserRepository $userRepository): Response
+    public function index(CategoryRepository $categoryRepository, UserRepository $userRepository): Response
     {
         if ($this->getUser() != null) {
             if ($this->getUser()->getRoles() === ["ROLE_USER", "ROLE_ADMIN"]) {
@@ -24,5 +26,14 @@ class CategoryController extends AbstractController
         }else{
             return $this->redirectToRoute('app_login');
         }
+    }
+
+    #[Route('/{category_label}', name: 'app_category_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        return $this->render('category/show.html.twig'/*, [
+            'category' => $category,
+            'files' => $fileRepository->findByUserAndCategory('tesJkU8 ', $category_label)
+        ]*/);
     }
 }
