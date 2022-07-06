4273ef89e5817c500ed7c74749f04245b89ddb94
Adélaïde LOUIS <adelaide.louis78@hotmail.fr>
2022-07-06 22:00:54 +0200

    edit fixture

--- espace_client/src/Controller/AdminController.php
+++ espace_client/src/Controller/AdminController.php
@@ -15,7 +15,6 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
