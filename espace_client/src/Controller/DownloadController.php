88a89993853a8e364e8ee269f6d893e2ca5905e6
PlayDuff <72228005+PlayDuff@users.noreply.github.com>
2022-07-06 16:57:35 +0200

    Download Fichier

--- espace_client/src/Controller/DownloadController.php
+++ espace_client/src/Controller/DownloadController.php
@@ -2,41 +2,21 @@
 
 namespace App\Controller;
 
-use App\Entity\File;
-use App\Entity\User;
-use App\Form\FileType;
-use App\Form\UserType;
-use App\Repository\CategoryRepository;
 use App\Repository\FileRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\Exception\ORMException;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Routing\RouterInterface;
-use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpKernel\KernelInterface;
 
-/*
- * @property KernelInterface $kernel
- */
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+
 #[Route('/download')]
 class DownloadController extends AbstractController
 {
     #[Route('/{id}', name: 'app_admin_file_download', methods: ['GET'])]
     public function downloadFile(int $id, FileRepository $fileRepository): BinaryFileResponse
     {
-        $path = str_ireplace("..", $this->getParameter('kernel.project_dir'),  $fileRepository->findOneBy(['id' => $id])->getPath());
+        $path = str_ireplace("..", $this->getParameter('kernel.project_dir'), $fileRepository->findOneBy(['id' => $id])->getPath());
         return $this->file($path);
 
-}
+    }
 }
